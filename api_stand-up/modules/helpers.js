e23367d721ce9b9192bef49f700618ed2f0b32f8
apkoha <79258179+apkoha@users.noreply.github.com>
2023-11-24 22:38:25 +0300

    helpers.js

--- api_stand-up/modules/helpers.js
+++ api_stand-up/modules/helpers.js
@@ -1,13 +1,16 @@
-export const sendData = (res, data) => {
-  res.writeHead(200, {
-    "Content-Type": "text/json; charset=utf-8",
-  });
-  res.end(JSON.stringify(data));
-};
+export const readRequestBody = (req) =>
+  new Promise((resolve, reject) => {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
 
-export const sendError = (res, statusCode, errMessage) => {
-  res.writeHead(statusCode, {
-    "Content-Type": "text/plain; charset=utf-8",
+    req.on("end", () => {
+      console.log(body);
+      resolve(body);
+    });
+
+    req.on("error", (err) => {
+      reject(err);
+    });
   });
-  res.end(errMessage);
-};
